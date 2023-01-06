b8f8b4ee618d4e185ebd1d7201350d5992ae7a6f
marcusPrado02 <silvamarcusprado@gmail.com>
2023-01-06 14:28:09 -0300

    src folder

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>É daqui que vamos comecar tudo</h2>
+    </div>
+  );
+}
+
+export default App;
