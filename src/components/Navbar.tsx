31d157ecca7d7aadaa2b0057aad5c76bb3e18eea
marcusPrado02 <silvamarcusprado@gmail.com>
2023-01-10 08:06:03 -0300

    Initial version Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,23 @@ export interface Props {
 const Navbar = (props: PropsWithChildren<Props>) => {
   return (
     <>
-      <h1>Esse é o Navbar de usuário</h1>
+      <header className=" text-white bg-blue-700 justify-center ">
+        <nav className="flex flex-row flex-nowrap p-4 justify-between">
+          <div className="basis-5/8 ml-2 space-x-10">
+            <img src="" alt="" />
+            <span>Aqui vai a logo</span>
+          </div>
+          <ul className="basis-3/8 flex flex-row-reverse text-sm text-center gap-4 ">
+            <li className="font-semibold">
+              <a href="">Your Account</a>
+            </li>
+            <li className="font-semibold">
+              <a href="">Log out</a>
+            </li>
+            <button></button>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 };
